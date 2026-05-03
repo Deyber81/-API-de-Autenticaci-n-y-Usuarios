bb1b1ae6fe8b6587094af9c786991087b3b29d78
Deyber81 <191202@unamba.edu.pe>
2026-05-02 20:46:11 -0500

    Agregar clase TRefresh_tokens para la gestión de tokens de refresco en el sistema de autenticación

---
+++ src/main/java/api/login_jwt/entity/TRefresh_tokens.java
@@ -0,0 +1,50 @@
+package api.login_jwt.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UuidGenerator;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TRefresh_tokens {
+    @Id
+    @UuidGenerator
+    @Column(name = "id", columnDefinition = "CHAR(36)", nullable = false, updatable = false)
+    private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private TUsuario usuario;
+
+    @Column(name = "token_hash", length = 255, nullable = false)
+    private String tokenHash;
+
+    @Column(name = "fecha_expiracion", nullable = false)
+    private LocalDateTime fechaExpiracion;
+
+    private Boolean revocado = false;
+
+    @Column(name = "fecha_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    protected boolean isExpired() {
+        return LocalDateTime.now().isAfter(fechaExpiracion);
+    }
+
+    public boolean isActive() {
+        return !revocado && !isExpired();
+    }
+
+}
