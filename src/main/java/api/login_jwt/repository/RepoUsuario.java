416e529f3122ed1100bba7040ff6f3d43a886d0c
Deyber81 <191202@unamba.edu.pe>
2026-05-04 12:15:40 -0500

    Agregar repositorio RepoUsuario para gestionar usuarios en el sistema de autenticación

---
+++ src/main/java/api/login_jwt/repository/RepoUsuario.java
@@ -0,0 +1,12 @@
+package api.login_jwt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import api.login_jwt.entity.TUsuario;
+
+@Repository
+public interface RepoUsuario extends JpaRepository<TUsuario, String> {
+    boolean existsByEmail(String email);
+
+}
