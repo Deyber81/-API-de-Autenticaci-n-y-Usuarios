cf521de4c4ed9f287d12feea8569b45f42fc8a83
Deyber81 <191202@unamba.edu.pe>
2026-05-12 17:33:24 -0500

    Agregar campos id, nombre, email, rol, token, refreshToken y expiresIn a la clase ResponseAuth

--- src/main/java/api/login_jwt/dto/auth/response/ResponseAuth.java
+++ src/main/java/api/login_jwt/dto/auth/response/ResponseAuth.java
@@ -1,5 +1,18 @@
 package api.login_jwt.dto.auth.response;
 
-public class ResponseAuth {
+import api.login_jwt.entity.TUsuario.Rol;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
+public class ResponseAuth {
+    private String id;
+    private String nombre;
+    private String email;
+    private Rol rol;
+    private String token;
+    private String refreshToken;
+
+    private Long expiresIn;
 }
